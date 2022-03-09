c265b671091fce6cf5e20fca651ac3c6d6ede432
Ericles Willian <83819706+ewnsilva@users.noreply.github.com>
2022-03-09 15:07:44 -0300

    Create todo_list.js

---
+++ todo_list.js
@@ -0,0 +1,45 @@
+const idendifyButton = document.getElementById("identifyButton")
+const taskValue = document.getElementById("inputText")
+const divTasks = document.getElementById("tasks")
+idendifyButton.addEventListener("click", newTask)
+
+function newTask () {
+    console.log(taskValue.value)
+
+    const div = document.createElement("div")
+
+    const inputCheckBox = document.createElement("input")
+    inputCheckBox.type = "checkbox"
+
+    const p = document.createElement("p")
+    p.innerText = taskValue.value
+    document.getElementById("inputText").value = ""
+
+    const inputDeleteButton = document.createElement("button")
+    inputDeleteButton.id = "deleteButton"
+    inputDeleteButton.classList.add("task__delete")
+    inputDeleteButton.innerText = "excluir"
+    inputDeleteButton.addEventListener("click", deleteTask)
+
+    /*const inputModifyButton = document.createElement("button")
+    inputModifyButton.id = "modifyButton"
+    inputModifyButton.classList.add("tasks__modify")
+    inputModifyButton.innerText = "modificar"
+    inputModifyButton.addEventListener("click", modifyTask)*/
+
+    div.appendChild(inputCheckBox)
+    div.appendChild(p)
+    /*div.appendChild(inputModifyButton)*/
+    div.appendChild(inputDeleteButton)
+    divTasks.appendChild(div)
+    
+}
+
+function deleteTask (e) {
+    const div = e.target.parentNode
+    div.innerHTML = ""
+}
+
+function modifyTask () {
+    
+} 
