c6266ea02f8aab279b8f9cb65ed2299d16e70e3c
Ericles Willian <83819706+ewnsilva@users.noreply.github.com>
2022-03-14 14:59:56 -0300

    Alterações no codigo HTML para inserir linhas PHP e acrescentar Banco de Dados. O Codigo PHP foi feito até a parte de acionar o botão inserir e permitir que o programa mostre o registo de dados armazenados no banco de dados.

---
+++ todo_list_conexao.php
@@ -0,0 +1,12 @@
+<?php
+
+    $hostname = "localhost";
+    $user = "root";
+    $password = "";
+    $database = "todo_list";
+    $conexao = mysqli_connect($hostname,$user,$password,$database);
+
+    /* if(!$conexao){
+        print "Falha na conexao com o Banco de Dados."
+    } */
+?>
